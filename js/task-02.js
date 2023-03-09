7eb131f744664d6d632ac2764785ee3217f9301b
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-03-09 12:12:24 +0200

    update task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+// console.log(ingredientsRef);
+
+const ingredientsArr = ingredients.map(ingredient => {
+  // console.log(ingredient);
+  const listItem = document.createElement('li');
+  listItem.classList.add('item');
+  listItem.textContent = ingredient;
+  return listItem;
+});
+
+ingredientsRef.append(...ingredientsArr);
