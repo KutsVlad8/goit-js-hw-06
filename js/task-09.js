7df0b7d2aa4d663bd262962ca048a8f8832beeb4
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-03-11 14:05:05 +0200

    update task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+const bodyRef = document.querySelector('body');
+const textColorRef = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+// console.dir(bodyRef.style);
+// console.log(textColorRef.textContent);
+// console.log(changeColorBtn);
+
+changeColorBtn.addEventListener('click', onChangeColorBody);
+
+function onChangeColorBody() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  textColorRef.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
