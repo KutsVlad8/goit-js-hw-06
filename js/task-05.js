2fb019aeb3c19f2ecc987b335206717649f5f460
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-03-11 13:12:59 +0200

    update task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,14 @@
 const textInputRef = document.querySelector('#name-input');
 const changeNameRef = document.querySelector('#name-output');
 
-// console.log(textInputRef);
+console.dir(textInputRef);
 // console.log(changeNameRef);
 
-textInputRef.addEventListener(
-  'input',
-  event => (changeNameRef.textContent = event.currentTarget.value),
-);
+textInputRef.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+  if (event.currentTarget.value === '') {
+    changeNameRef.textContent = 'Anonymous';
+  }
+  changeNameRef.textContent = event.currentTarget.value;
+}
