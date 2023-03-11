c2e448c5190c9433b7971dd82f31d4074ae783a0
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-03-11 22:56:34 +0200

    update task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
+const controlsDivRef = document.querySelector('#controls');
+const inputRef = document.querySelector('input');
+// const inputRef = controlsDivRef.firstChild;
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesRef = document.querySelector('#boxes');
+
+console.log(controlsDivRef);
+console.log(inputRef);
+console.log(createBtn);
+console.log(destroyBtn);
+console.log(boxesRef);
+
+inputRef.addEventListener('input', onChangeInput);
+
+function onChangeInput(event) {
+  console.dir(event.currentTarget.value);
+}
+
+createBtn.addEventListener('click', onCreateBoxes);
+
+function onCreateBoxes() {
+  console.log('Vlad');
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
